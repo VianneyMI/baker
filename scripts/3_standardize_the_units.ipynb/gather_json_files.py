3ddfafa9f6651b53c77b86c3d487d57f216b53f3
Vianney Mixtur <vianney.mixtur@outlook.fr>
2024-11-26 14:02:28 +0100

    Retructured repo

--- scripts/3_standardize_the_units.ipynb/gather_json_files.py
+++ scripts/3_standardize_the_units.ipynb/gather_json_files.py
@@ -5,18 +5,33 @@ import glob
 from pathlib import Path
 
 # Step 1: Get all the JSON file paths
-file_paths = glob.glob(str(Path(__file__).parents[1] / "data" / "output" / "2_update_the_extraction_results" / "main_run") + "/*.json")
+file_paths = glob.glob(
+    str(
+        Path(__file__).parents[2]
+        / "data"
+        / "output"
+        / "2_update_the_extraction_results"
+        / "main_run"
+    )
+    + "/*.json"
+)
 
 # Step 2: Load each JSON document and add to a list
 json_documents = []
 for file_path in file_paths:
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         data = json.load(file)
         json_documents.append(data)
 
 # Step 3: Save the list of JSON documents as a single JSON array
-output_path = Path(__file__).parents[1] / "data" / "output" / "3_standardize_the_data" / "parsed_recipes.json"
-with open(output_path, 'w') as output_file:
+output_path = (
+    Path(__file__).parents[2]
+    / "data"
+    / "output"
+    / "3_standardize_the_data"
+    / "parsed_recipes.json"
+)
+with open(output_path, "w") as output_file:
     json.dump(json_documents, output_file, indent=4)
 
 print(f"Bundled JSON saved to {output_path}")
