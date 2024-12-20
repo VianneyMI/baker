b116e421e983d16a5cc120872267e344146cc770
Vianney Mixtur <vianney.mixtur@outlook.fr>
2024-12-20 17:36:56 +0100

    V0 of query for the core engine

--- src/baker/engine/core.py
+++ src/baker/engine/core.py
@@ -15,7 +15,7 @@ def get_recipes_collection() -> Collection:
     return recipes
 
 
-def find_recipes(ingredients: list[Ingredient]) -> dict:  # recipes
+def find_recipes(ingredients: list[Ingredient]) -> list[dict]:  # recipes
     """Find recipes."""
 
     # Get the recipes collection
@@ -23,8 +23,30 @@ def find_recipes(ingredients: list[Ingredient]) -> dict:  # recipes
 
     # Create the pipeline
     pipeline = Pipeline()
+    query = generate_match_query(ingredients)
+    # TODO: Omitting normalization for now
+    pipeline.match(query=query)
 
     # Find the recipes
-    result = recipes.aggregate(pipeline)
+    result = recipes.aggregate(pipeline.export()).to_list(length=None)
 
     return result
+
+
+def generate_match_query(ingredients: list[Ingredient]) -> dict:
+    """Generate the match query."""
+
+    operands = []
+    for ingredient in ingredients:
+        operand = {
+            "ingredients.name": ingredient.name,
+            "ingredients.unit": ingredient.unit,
+            "ingredients.quantity": S.lte(
+                S.field("ingredients.quantity"), ingredient.quantity
+            ),
+        }
+        operands.append(operand)
+
+    query = {"$and": operands}
+
+    return query
