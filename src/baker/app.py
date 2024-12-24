29e72974a10455725a2f66388f2b1a0e9e7caa08
Vianney Mixtur <vianney.mixtur@outlook.fr>
2024-12-24 10:43:37 +0100

    Removed temporary return typehint on endpoint

--- src/baker/app.py
+++ src/baker/app.py
@@ -15,7 +15,7 @@ def read_root():
 
 
 @app.post("/recipes")
-def _find_recipes(ingredients: list[Ingredient], serving_size: int = 1) -> list[Recipe]:
+def _find_recipes(ingredients: list[Ingredient], serving_size: int = 1) -> list[dict]:
     """Find recipes"""
 
     return find_recipes(ingredients, serving_size)  # type: ignore
--- src/baker/models/recipe.py
+++ src/baker/models/recipe.py
@@ -1,6 +1,7 @@
 """`baker.models.recipe` module."""
 
 from pydantic import BaseModel
+from baker.schemas.range import Range
 from baker.models.ingredient import Ingredient
 
 
@@ -11,8 +12,8 @@ class Recipe(BaseModel):
     """
 
     title: str
-    serving_size: int = 1
-    preparation_time: float | None = None
-    cooking_time: float | None = None
+    serving_size: int | None = None
+    preparation_time: float | Range | None = None
+    cooking_time: float | Range | None = None
     ingredients: list[Ingredient]
     directions_source_text: str | None = None
